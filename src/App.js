6831060ec9d180f8d454ceee653057001e511678
Anton <foxic@foxic.ru>
2021-03-26 00:06:21 +0300

    React App Init. Layout WIP.

---
+++ src/App.js
@@ -0,0 +1,155 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div id="app">
+      <aside>
+        <div className="e1_156"></div>
+        <div className="e1_157">
+          <div className="e1_158"></div>
+          <div className="e1_159"></div>
+        </div>
+        <div className="e1_160">
+          <div className="e1_161"></div>
+          <div className="e1_162"></div>
+        </div>
+        <div className="e1_163">
+          <div className="e1_164"></div>
+          <div className="e1_165"></div>
+        </div>
+        <div className="e1_166">
+          <div className="e1_167"></div>
+          <div className="e1_168"></div>
+        </div>
+        <div className="e1_169">
+          <div className="e1_170"></div>
+          <div className="e1_171"></div>
+        </div>
+        <div className="e1_172">
+          <div className="e1_173"></div>
+          <div className="e1_174"></div>
+        </div>
+        <div className="e1_175">
+          <div className="e1_176"></div>
+          <div className="e1_177"></div>
+        </div>
+        <div className="e1_178">
+          <div className="e1_179"></div>
+          <div className="e1_180"></div>
+        </div>
+        <div className="e1_181">
+          <div className="e1_182"></div>
+          <div className="e1_183"></div>
+        </div>
+      </aside>
+      <main>
+        <header>
+          <div className="e1_185"></div>
+          <div className="e1_186"><span className="e1_187">159 465 $</span><span className="e1_188">Payout</span></div>
+          <div className="e1_189"><span className="e1_190">213 920 $</span><span className="e1_191">Balance</span></div>
+        </header>
+        <span className="e1_192">Services</span>
+        <div className="e1_193"><span className="e1_194">Filter</span>
+          <div className="e1_195"></div>
+          <div className="e1_196"></div>
+          <span className="e1_197">Se</span>
+          <div className="e1_198"></div>
+        </div>
+        <div className="e1_199">
+          <div className="e1_200"></div>
+          <span className="e1_201">Reset</span></div>
+        <div className="e1_202">
+          <div className="e1_203">
+            <div className="ei1_203_3613_82"></div>
+          </div>
+          <div className="e1_204"></div>
+          <div className="e1_205"><span className="e1_206">Promocode</span>
+            <div className="e1_207">
+              <div className="e1_208"></div>
+              <span className="e1_209">itpromocodes</span>
+              <div className="e1_210">
+                <div className="ei1_210_1069_78"></div>
+              </div>
+            </div>
+          </div>
+          <div className="e1_211">
+            <div className="ei1_211_4923_369">
+              <div className="ei1_211_4923_369_4923_284"></div>
+            </div>
+            <span className="ei1_211_892_707">Activate bonus</span></div>
+          <span className="e1_212">Sitecostructor.io</span><span className="e1_213">Description</span></div>
+        <div className="e1_214">
+          <div className="e1_215">
+            <div className="ei1_215_3613_82"></div>
+          </div>
+          <div className="e1_216"></div>
+          <div className="e1_217"><span className="e1_218">Promocode</span>
+            <div className="e1_219">
+              <div className="e1_220"></div>
+              <span className="e1_221">itpromocodes</span>
+              <div className="e1_222">
+                <div className="ei1_222_1069_78"></div>
+              </div>
+            </div>
+          </div>
+          <div className="e1_223">
+            <div className="ei1_223_4923_369">
+              <div className="ei1_223_4923_369_4923_284"></div>
+            </div>
+            <span className="ei1_223_892_707">Activate bonus</span></div>
+          <span className="e1_224">Appvision.com</span><span className="e1_225">Description</span></div>
+        <div className="e1_226">
+          <div className="e1_227">
+            <div className="ei1_227_3613_82"></div>
+          </div>
+          <div className="e1_228"></div>
+          <span className="e1_229">Analytics.com</span><span className="e1_230">Description</span>
+          <div className="e1_231"><span className="e1_232">Promocode</span>
+            <div className="e1_233">
+              <div className="e1_234"></div>
+              <span className="e1_235">itpromocodes</span>
+              <div className="e1_236">
+                <div className="ei1_236_1069_78"></div>
+              </div>
+            </div>
+          </div>
+          <div className="e1_237">
+            <div className="ei1_237_4923_369">
+              <div className="ei1_237_4923_369_4923_284"></div>
+            </div>
+            <span className="ei1_237_892_707">Activate bonus</span></div>
+        </div>
+        <div className="e1_238">
+          <div className="e1_239">
+            <div className="ei1_239_3613_82"></div>
+          </div>
+          <div className="e1_240"></div>
+          <div className="e1_241"><span className="e1_242">Promocode</span>
+            <div className="e1_243">
+              <div className="e1_244"></div>
+              <span className="e1_245">itpromocodes</span>
+              <div className="e1_246">
+                <div className="ei1_246_1069_78"></div>
+              </div>
+            </div>
+          </div>
+          <div className="e1_247">
+            <div className="ei1_247_4923_369">
+              <div className="ei1_247_4923_369_4923_284"></div>
+            </div>
+            <span className="ei1_247_892_707">Activate bonus</span></div>
+          <span className="e1_248">Logotype</span><span className="e1_249">Description</span></div>
+        <div className="e1_250"><span className="e1_251">© IT Promocodes, 2019</span>
+          <div className="e1_252">
+            <div className="ei1_252_5468_5770">
+              <div className="ei1_252_5468_5770_4457_0"></div>
+            </div>
+          </div>
+        </div>
+      </main>
+    </div>
+  );
+}
+
+export default App;
